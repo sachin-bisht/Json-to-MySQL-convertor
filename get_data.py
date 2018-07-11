783fb822591613bd72087cf40834ae4273c6e9a5
Sachin Bisht <sachinbisht939@gmail.com>
2018-07-11 12:26:30 +0530

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -1,8 +1,7 @@
 import requests
 
 def get_data():
-	# 'https://sanction.seva.capitalfloat.com/v1/sanction/73764305-0be7-4e28-8f56-d20176148824'
-	url = 'https://sanction.seva.capitalfloat.com/v1/sanction/73764305-0be7-4e28-8f56-d20176148824'
+	url = 'ENTER URL'
 
 	r = requests.get(url)
 	data = r.json()
